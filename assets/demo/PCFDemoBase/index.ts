30b8fd5ef4ba0210950c1617a07b13b8d5f51a26
Andreas Aschauer <andreas.aschauer@microsoft.com>
2023-06-28 12:09:37 +0000

    Added Post demo

--- assets/demo/PCFDemoBase/index.ts
+++ assets/demo/PCFDemoBase/index.ts
@@ -1,6 +1,6 @@
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
 
-export class LinearInputControl
+export class PostLinearInput
 implements ComponentFramework.StandardControl<IInputs, IOutputs>
 {
 private _value: number;
@@ -9,7 +9,7 @@ private labelElement: HTMLLabelElement;
 private inputElement: HTMLInputElement;
 private _container: HTMLDivElement;
 private _context: ComponentFramework.Context<IInputs>;
-private _refreshData: EventListenerOrEventListenerObject;
+
 /**
    * Empty constructor.
 */
@@ -40,13 +40,13 @@ public init(
    this._context = context;
    this._container = document.createElement("div");
    this._notifyOutputChanged = notifyOutputChanged;
-   this._refreshData = this.refreshData.bind(this);
+
 
    // creating HTML elements for the input type range and binding it to the function which 
    // refreshes the control data
    this.inputElement = document.createElement("input");
    this.inputElement.setAttribute("type", "range");
-   this.inputElement.addEventListener("input", this._refreshData);
+
 
    //setting the max and min values for the control.
    this.inputElement.setAttribute("min", "1");
@@ -125,6 +125,6 @@ public getOutputs(): IOutputs {
 */
 public destroy(): void {
    // Add code to cleanup control if necessary
-   this.inputElement.removeEventListener("input", this._refreshData);
+
       }
 }
